14afd7af94ebc46d7b3b695e9d42bbc17bd07eda
WUP_MON\CzyzM <czyz.m@eplan.pl>
2021-06-28 14:30:04 +0200

    wrap, invoke

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -63,13 +63,86 @@ describe('Our first suite', ()  => {
 
     })
 
-    it.only('then and wrap methods', () => {
+    it('then and wrap methods', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
-        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        //selenium
+        // const firstForm = cy.contain(nb, ab)
+        // const secondForm = cy.contains(nb, ba)
+
+        // firstForm.find('nb fff')
+
+        //cypress style
+
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst)
+
+                //konwertowanie do cypress format .wrap
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+            })
+        })
     })
+
+    it('invoke command', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            expect(label.text()).to.equal('Email address')
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            .should('contain', 'checked') //first
+            .then(classValue => { //second
+                expect(classValue).to.contain('checked')
+            })
+
+    })
+
+    it.only('assert property', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', "Common Datepicker")
+            .find('input')
+            .then(input => {
+                cy.wrap(input).click()
+                cy.get('nb-calendar-day-picker').contains('17').click()
+                cy.wrap(input).invoke('prop', 'value').should('contain', 'Jun 17, 2021')
+
+            })
+    })
+
     
 })
 
