d87b7b1a8f0a3bf8952e7aab38cbf4c79b501739
WUP_MON\CzyzM <czyz.m@eplan.pl>
2021-07-07 15:32:42 +0200

    Dialog box

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -128,7 +128,7 @@ describe('Cypress nauka', ()  => {
 
     })
 
-    it.only('assert property, date, funkcje, ify parametry', () => {
+    it('assert property, date, funkcje, ify parametry', () => {
 
         function selectDayFromCurrent(day){
             let date = new Date()
@@ -273,6 +273,43 @@ describe('Cypress nauka', ()  => {
 
         })
     })
+
+    it('Tooltip', () =>{
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+
+        cy.contains('nb-card', 'Colored Tooltips')
+            .contains('Default').click()
+        cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+
+    })
+    it.only('Dialog box', () =>{
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', (confirm) =>{
+            expect(confirm).to.equal('Are you sure you want to delete?')
+        })
+
+        //2
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        })
+
+         //3
+         cy.get('tbody tr').first().find('.nb-trash').click()
+         cy.on('window:confirm', () => false)
+         })
+
+
+    })
+
 
 
 
